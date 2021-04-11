652010cdfa2104fa8123e1441e5a9eeef4902405
Moritz Schütz <moritz-schuetz@gmx.de>
2021-04-11 16:48:33 +0200

    Initial commit

---
+++ Assets/GPSPrinter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPSPrinter : MonoBehaviour
+{
+    LocationInfo locI = new LocationInfo();
+    void Start()
+    {
+        this.GetComponent<TestLocationService>();
+    }
+    private void FixedUpdate()
+    {
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            locI = Input.location.lastData;
+            Debug.Log(Input.location.lastData);
+        }
+    }
+    
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 100, 100), $"L: {locI.latitude} | L: {locI.longitude}");
+    }
+}
